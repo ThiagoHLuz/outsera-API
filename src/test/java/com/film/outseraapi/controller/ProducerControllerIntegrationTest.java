73dd3cf2478284a7447a9894bea147e48af6d28a
Thiago Henrique Luz <79991511+ThiagoHLuz@users.noreply.github.com>
2024-07-27 23:44:56 -0300

    Configuração testes

--- src/test/java/com/film/outseraapi/controller/ProducerControllerIntegrationTest.java
+++ src/test/java/com/film/outseraapi/controller/ProducerControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,6 +20,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class ProducerControllerIntegrationTest {
 
     private MockMvc mockMvc;
